7b2d0a834fa9b0c17b16999cf89b24b86ee6ef5b
root <hold.the.dianhua@gmail.com>
2022-09-09 15:24:57 -0700

    exlcuding q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print all lowercase
+ * excluding q and e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
