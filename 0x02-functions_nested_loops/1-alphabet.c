c9c93198883cda2d6b313b58af1d0467e13b1746
root <hold.the.dianhua@gmail.com>
2022-09-13 06:10:11 -0700

    moving to vagrant alaphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print alphabet: in lowercase
+ * followed by a new line
+ * Return: void
+ */
+void print_alphabet(void)
+{
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,3 @@
 int main(void);
 int _putchar(char a);
+void print_alphabet(void);
