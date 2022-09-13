118f60710759d676b18a6e58992f1f219cc561e3
khumilla <hold.the.dianhua@gmail.com>
2022-09-13 18:43:51 +0000

    errors

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
 /**
- * print alphabet: in lowercase
+ * print_alphabet- a to z in lowercase
  * followed by a new line
  * Return: void
  */
 void print_alphabet(void)
 {
+	char a = 'a';
+	char z = 'z';
+
+	while
+		(a <= z);
+	{
+		_putchar (a);
+		a++;
+	}
+	_putchar ('\n');
+	return (void);
+}
