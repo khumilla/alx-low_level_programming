e79787a7456057eafc241951d878794ff70ce174
khumilla <hold.the.dianhua@gmail.com>
2022-09-14 12:13:33 +0000

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,10 +3,14 @@
 /**
  * _isalpha - checks for alphabetic character
  * @c: a character to check
- * Return: 1 if c is a letter, upper or lower
- * Return: 0 otherwise
+ * Return: 1 if c is a letter, upper or lower 0 otherwise
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z' || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
