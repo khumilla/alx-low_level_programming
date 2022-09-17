b96eef25915748b8a39afeec302451e80655dcf3
khumilla <hold.the.dianhua@gmail.com>
2022-09-17 19:25:40 +0000

    try most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers 0-9 excluding 2 and 4
+ * Return: everything but 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	if (n != 2 && n != 4)
+	{
+		_putchar((n % 10) + 0);
+	}
+	}
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -8,4 +8,6 @@ int _isdigit(int c);
 int mul(int a, int b);
 int main(void);
 void print_numbers(void);
+void print_most_numbers(void);
+
 #endif
