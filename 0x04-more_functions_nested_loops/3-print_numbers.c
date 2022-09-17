1344d64fc4f77cb8cb037b7a7d227ee6df2f1f46
khumilla <hold.the.dianhua@gmail.com>
2022-09-17 18:54:17 +0000

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0-9
+ * Return: printed numbers
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
+}
