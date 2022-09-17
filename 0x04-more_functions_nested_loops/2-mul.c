92d99102b942fc641f4a11028203112c7bbda46a
khumilla <hold.the.dianhua@gmail.com>
2022-09-17 18:05:10 +0000

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplying two integers together
+ * @a: first integer
+ * @b: second integer
+ * Return: success the product of both integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,7 +2,15 @@
 #define MAIN_H
 #include <stdio.h>
 
+/**
+ * @a: integer
+ * @b: integer
+ */
+
 int _isupper(int c);
 int _putchar (char c);
 int _isdigit(int c);
+int mul(int a, int b)
+int main(void);
+
 #endif
