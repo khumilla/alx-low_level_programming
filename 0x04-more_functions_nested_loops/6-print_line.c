8fcdd132eb297d57f91588c634ecbd7ed8f1497a
khumilla <hold.the.dianhua@gmail.com>
2022-09-18 08:56:08 +0000

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - print character _ to print a stragiht line
+ * @n: number of characters to print
+ * Return: _ is n is greater than 0, new line otherwise
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+	for (line = 0; line <= n; line++)
+	{
+		_putchar('_');
+	}
+	}
+	_putchar('\n');
+}
