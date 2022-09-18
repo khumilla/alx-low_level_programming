d04ff3a78b2bdb0eb50f88af21b1565449dcc2e4
khumilla <hold.the.dianhua@gmail.com>
2022-09-18 14:15:01 +0000

    omit 2

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,11 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
-	
 	{
-	while (n != 2 && n != 4)
-	{
-		_putchar(n);
+		if ((n == 2) || ( n == 4))
+			continue;
+		else
+			_putchar(n + '0');
 	}
 	_putchar('\n');
-	}
 }
