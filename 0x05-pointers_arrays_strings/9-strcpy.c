3b0ce8cb019f592d617e4addd0eabd6651b16756
khumilla <hold.the.dianhua@gmail.com>
2022-09-21 03:33:44 +0000

    source copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * strcpy - copy string pointed to by src
+ * @dest: destination
+ * @src: source code
+ * Return: the pointner to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + i) = '\0';
+
+	return (dest);
+}
