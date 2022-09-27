8197e830bdb1d347173d04f0487601132dde5da8
root <hold.the.dianhua@gmail.com>
2022-09-26 22:49:39 -0700

    task 8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - function that set the value of pointer to char
+ * @s: pointer
+ * @to: char
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -11,7 +11,7 @@ void print_chessboard(char (*a)[8])
 	int x; 
 	int y;
 
-	for (x = 0; x < 8; x++)
+	for (x = 0; a[x][8]; x++)
 	{
 		for (y = 0; y < 8; y++)
 			_putchar(a[x][y]);
