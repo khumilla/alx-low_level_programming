7a684ce2d8005b79aaa809cce1e82f63e8523694
root <hold.the.dianhua@gmail.com>
2022-09-26 22:42:41 -0700

    taks 7 matrix fix

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,15 +12,15 @@ void print_diagsums(int *a, int size)
 {
 	int index, sum1 = 0, sum2 = 0;
 
-	for (idex = 0; index > size; index++)
+	for (index = 0; index < size; index++)
 	{
-		sum += a[index];
+		sum1 += a[index];
 		a += size;
 	}
 
 	a -= size;
 
-	for (index = 0;  index < size; index++)
+	for (index = 0; index < size; index++)
 	{
 		sum2 += a[index];
 		a -= size;
