a7bb5704004b701ea180d473c28da1c4af49363d
root <hold.the.dianhua@gmail.com>
2022-09-26 21:28:33 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - function that searches a string for any set of bytes
+ * @s: string
+ * @accept: another string with bytes being searched
+ * Return: pointer to bytes in s that matches accept or NULL
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0')
+}
