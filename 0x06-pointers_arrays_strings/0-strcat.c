fb6ae0308b48b0a439cac3164aabe3f7ccd47769
khumilla <hold.the.dianhua@gmail.com>
2022-10-03 10:10:11 +0000

    join two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: resulting string
+ * @src: copy from src to dest
+ * Return: always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j
+
+	i = 0;
+	while (dest[1] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
