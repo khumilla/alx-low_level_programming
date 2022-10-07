4ba40962b233c5ab9df67ed2021c3dd8fa10ccfa
khumilla <hold.the.dianhua@gmail.com>
2022-10-07 23:18:24 +0000

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: max number of bytes in s2 to s1
+ * Return: newly allocated pointer of s1 and s2 and '\0'
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int len = n, i;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i]; i++)
+		len++;
+
+	p = malloc(sizeof(char) * (len + 1));
+
+	if (p == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (i = 0; s1[i]; i++)
+		p[len++] = s1[i];
+
+	for (index = 0; s2[i] && i < n; i++)
+		p[len++] = s2[i];
+
+	p[len] = '\0';
+
+	return (p);
+}
