c547500252df8bea616e81b6e6f579c9b56fcf99
khumilla <hold.the.dianhua@gmail.com>
2022-10-10 13:55:32 +0000

    task 9 fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,30 +14,25 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 != 0)
+	for (i = 1; i <= 100; i++)
+{
+		if (i % 3 == 0 || i % 5 == 0)
 		{
-			printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+		} else
 		{
 			printf("%d", i);
 		}
-		else
-		{
-			printf(" %d", i);
-		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
-
 	return (0);
 }
