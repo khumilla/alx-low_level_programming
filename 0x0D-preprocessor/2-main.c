f0a66b889ae3cacd5ef900d0582581ab7c70c61c
khumilla <hold.the.dianhua@gmail.com>
2022-10-10 18:33:56 +0000

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - function to print name of file it is compiled from
+ *
+ * Return: name of program
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+
