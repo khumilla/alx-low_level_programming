ac48a556c3ef94df7c9fe42e48f9035b3d0e81c6
khumilla <hold.the.dianhua@gmail.com>
2022-10-10 07:43:36 +0000

    task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,20 @@ void print_diagonal(int n)
 {
         int line, space;
 
-        if ((n == 0) && (n < 0))
-        {
-                _putchar('\n');
-        }
-                for (space = 1; space <= n; space++)
-		{	
-			_putchar(' ');
-		
-			for (line = 0; line <= n; line++)
-			{
-				_putchar(92);
-			}
+        if (n <= 0)
+	{
 		_putchar('\n');
+	}
+	else
+	{
+		for (line = 0; line < n; line++)
+		{
+			for (space = 0; space < line; space++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-        _putchar('\n');
+	}
 }
