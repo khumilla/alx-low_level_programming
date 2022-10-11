d88e525ea524e079f9e8e82847ead19f867751a8
khumilla <hold.the.dianhua@gmail.com>
2022-10-11 00:03:31 +0000

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - new struct type for dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: 0
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
