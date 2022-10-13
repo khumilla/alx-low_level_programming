99dbec76ce3962ce7c0ec8cfab675eabab1d8258
khumilla <hold.the.dianhua@gmail.com>
2022-10-13 22:31:52 +0000

    task 2 fix fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,13 +1,13 @@
-#include "function_pointer.h"
+#include "function_pointers.h"
 
 /**
  * int_index - function that searches for an integer
  * @array: array of elements
  * @size: number of elements in array
  * @cmp: pointer to function to compare values.
- * 
+ *
  * Return: index of first element != 0
- * if no element matches -1 
+ * if no element matches -1
  * or size if <= 0; -1.
  */
 
