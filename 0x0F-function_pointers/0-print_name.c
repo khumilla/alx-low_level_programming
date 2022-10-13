087754f86eab2edba8a11b2aa329396f8939aaa0
khumilla <hold.the.dianhua@gmail.com>
2022-10-13 00:12:17 +0000

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#inlcude "function_pointers.h"
+
+/**
+ * print_name - function that prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ *
+ * Return: always 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
