0d2e32c48b49054c9ab62939764424db2d80b713
khumilla <hold.the.dianhua@gmail.com>
2022-12-08 20:25:07 +0000

    task 0 new PAT

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,12 @@
+
+
 #include "lists.h"
 
 /**
- * print_dlistint - print all the elements of dlistint_t
- * @h: head of the dlistint_t list
- * Return: the numbers of nodes in the list
+ * print_dlistint - Prints all the elements of a dlistint_t list.
+ * @h: The head of the dlistint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 
 size_t print_dlistint(const dlistint_t *h)
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -5,20 +5,21 @@
 #include <stdio.h>
 
 /**
- * struct dlistint_s - doubly linked lists
+ * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
- * @next: point to the next node
+ * @next: points to the next node
  *
- * Description: doubly linked lists node structure
+ * Description: doubly linked list node structure
  *
  */
 typedef struct dlistint_s
 {
 	int n;
-	struct dlinstint_s *prev;
+	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
+
 
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
@@ -30,4 +31,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif /* LIST_H */
+#endif
