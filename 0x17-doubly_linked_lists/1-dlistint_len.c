4be51eeaf34048d928b9e7b8e5ac7c219a46f14d
khumilla <hold.the.dianhua@gmail.com>
2022-12-09 09:54:26 +0000

    task 1 fix

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,14 +7,13 @@
  * Return: The number of nodes in the list.
  */
 
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
 	const dlistint_t *tmp = h;
 
 	while (tmp != NULL)
 	{
-		printf("%d\n", tmp->n);
 		tmp = tmp->next;
 		i++;
 	}
