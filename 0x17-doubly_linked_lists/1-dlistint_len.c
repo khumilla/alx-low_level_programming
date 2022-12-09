66b1bf915a55a3c6126745e5c6aa9c983d2e8139
khumilla <hold.the.dianhua@gmail.com>
2022-12-09 11:56:19 +0000

    missing description

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - Prints all the elements of a dlistint_t list.
+ * dlistint - Prints all the elements of a dlistint_t list.
  * @h: The head of the dlistint_t list.
  *
  * Return: The number of nodes in the list.
